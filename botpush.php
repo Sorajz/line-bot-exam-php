2524aaa7b259524d6e06a9ef0d5e460907ff0a43
soraj <buasri_soraj@hotmail.com>
2017-11-16 18:04:14 +0700

    complete

--- botpush.php
+++ botpush.php
@@ -8,11 +8,13 @@ $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuX
 
 $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
+$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
