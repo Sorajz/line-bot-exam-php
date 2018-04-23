c6201ecc506ccf17dd19ea39fd846c0bc6c78bfe
nora09 <38667392+nora09@users.noreply.github.com>
2018-04-24 01:24:01 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['displayName'];
+			$text = $textReplyMessage = "ผมเป็นบอทเดิร์นครับ ผมยังหาคำสั่งให้ปริ้น displayname ไม่ได้ หาได้เเต่ userid คือ : ";
+			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
