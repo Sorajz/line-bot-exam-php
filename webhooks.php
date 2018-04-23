8853e87300c4f3e0698c923f7ba9f57a1c77aac5
nora09 <38667392+nora09@users.noreply.github.com>
2018-04-24 01:20:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+			$text = $event['source']['displayName'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
