84dfda9b73e27a9419542d08eec8564120c7242d
Krongkaew Ponboobpha <krongkaew@carro.co>
2018-11-28 11:55:58 +0700

    profile

--- webhooks.php
+++ webhooks.php
@@ -4,6 +4,7 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'xYWwce1Yov/D+hm3t44Kh2O56s+uoLtWJI6cp9pC/EU1Iw8JwXEoKW0ojBSOh4QRm9tvvd1bWAE8gV4pxQeB234RqRFsQGNBVUXo/OyS7grsUP0nUiNVpVzS+AEFJnE74HvEjG7jek6stse12PAztwdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '18c44ec2d7cbaeef30ffe0f331a33f2b';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -14,7 +15,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if (true) {
 			// Get text sent
 			$text = $event['source']['userId'];
 			// Get replyToken
@@ -44,7 +45,19 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";
+			echo $result + 'hahaha'. "\r\n";
+		} else if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			echo 'hahaha'. "\r\n";
+
+			$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+			$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+			$response = $bot->getProfile('<userId>');
+			if ($response->isSucceeded()) {
+				$profile = $response->getJSONDecodedBody();
+				echo $profile['displayName'];
+				echo $profile['pictureUrl'];
+				echo $profile['statusMessage'];
+			}
 		}
 	}
 }
