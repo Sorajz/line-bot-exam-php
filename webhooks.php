ba3c3e1920fe751093b0a114a33078acc9365532
alian3343 <ganok88@gmail.com>
2019-04-24 14:03:55 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '2S/wgd5DwuH0YG7swpnbAtMK6xJFmzVhkyai3qa5T/5Zo8ite5/GnsziSVbRqBP/eLx4pYrxoGOfCZy6g+EDUbG6FpHAnjaQhCsQ/MkEUorBqK/9OBzSWYip8VWcT/seI4+5IHVMpbXX6thYzZLaAAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
