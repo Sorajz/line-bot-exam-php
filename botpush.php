b68278528c82f71f659978dec3391ad5c1c100a7
m9nood <nattawoot@livingmobile.me>
2019-10-01 18:26:55 +0700

    update file index.php

--- botpush.php
+++ botpush.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 require "vendor/autoload.php";
 
 $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
