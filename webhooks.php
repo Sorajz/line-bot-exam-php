dcc18c9a7a351068450eb40c16a561378266a246
soft2you <55018475+soft2you@users.noreply.github.com>
2020-01-09 10:32:15 +0700

    Update webhooks.php
    add

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'UxIkzG4b69iRJKl6uCn4t7R+4SF7tqb2IrI3i1etzpCu5mOkeiHmCTrXLbmF1qRt+mRQp0QFDV2w67QUQCnFj41AsJCf1K1ZsqC4HasxLsnm0cVLG1lI37xY36W6wDkU5fpkTlmsLheR+voyHjaDlgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
