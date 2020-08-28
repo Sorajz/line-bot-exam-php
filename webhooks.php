2a3bb295c2a233b70b71586e8156153d1001b4e8
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-28 15:43:11 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'สวัสดี'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
