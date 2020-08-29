2b4c7f239a20493384347bae0a237eb449e9be58
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-29 12:45:32 +0700

    Update webhooksnew.php

--- webhooksnew.php
+++ webhooksnew.php
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, all);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
@@ -50,34 +50,4 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 echo "message auto send person all"
-	// Get text sent
-			//$text = $event['source']['userId'];
-			// Get replyToken
-			//$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'ALL'
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/broadcast';
-			$data = [
-				
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
 echo "message end"
