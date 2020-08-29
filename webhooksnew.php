59f0d9a50499c03820cc5de2985958f15b068cd7
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-29 19:43:10 +0700

    Update webhooksnew.php

--- webhooksnew.php
+++ webhooksnew.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			$userida = $event['source']['userId'];
-$responseprofile = $bot->getProfile('$userida');
+$responseprofile = $botchannelSecret->getProfile('$userida');
 if ($responseprofile->isSucceeded()) {
     $profile = $responseprofile->getJSONDecodedBody();
     echo $profile['displayName'];
