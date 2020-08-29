7a7583de77ed03115d1e05f6342612357a10c56b
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-29 14:49:42 +0700

    Update webhooksnew.php

--- webhooksnew.php
+++ webhooksnew.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['text'] == 'ไอดี') {
 			// Get text sent
 			$text = $event['source']['userId'];
 			// Get replyToken
