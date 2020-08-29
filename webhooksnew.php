74ab70ef1380515691ff59e784062a8679753ba9
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-29 12:49:42 +0700

    Update webhooksnew.php

--- webhooksnew.php
+++ webhooksnew.php
@@ -48,6 +48,28 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
-echo "message auto send person all";
+echo "OK<br>";
+echo "message auto send person all<br>";
+$messages = [
+				'type' => 'text',
+				'text' => 'สวัสดี'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/broadcast';
+			$data = [
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, all);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 echo "message end";
