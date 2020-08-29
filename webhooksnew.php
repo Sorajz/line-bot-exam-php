2920e3985db38ea718b5555aea4a07a91e776696
Srluboy <67352751+Srluboy@users.noreply.github.com>
2020-08-29 16:39:24 +0700

    Update webhooksnew.php

--- webhooksnew.php
+++ webhooksnew.php
@@ -34,7 +34,7 @@ if (!is_null($events['events'])) {
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'replyToken' => [$replyToken],
+				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
