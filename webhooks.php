4148885d3746b92d84409b810a73c7f8da0ffb71
zpcyn9412 <73181613+zpcyn9412@users.noreply.github.com>
2020-10-21 12:25:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'NF5OG3SFX5lB / ILlryuHiAEJQTkW6eA6jpx ++ Uffs7PXRqX / xMEcTOtc / 0 + + 8XeUuZT93CDuwe4FaFHrYW5aH 6hTF / wgXJoJdr4G7bJHq87kEH83neSR6Vlv9cZ6T7Vw3lECZejo1o + ryCl1r6weXgdB04t89 / 1O / w1cDnyilFU =';
 
 // Get POST body content
 $content = file_get_contents('php://input');
