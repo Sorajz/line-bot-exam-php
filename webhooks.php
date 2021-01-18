f24901dd7b198d7c570f0324ab8aad826fba1e0a
zpcyn9412 <73181613+zpcyn9412@users.noreply.github.com>
2021-01-18 19:48:45 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -49,3 +49,14 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
+<?php
+require "vendor/autoload.php";
+$access_token = 'fxte5XFm9gh9kGVkWM/PCcYuao7f5tOt6Iq8GAAXUCqJ2HfIZy2U33PK6r6WwO0CAv9Sul+mLr/fq8G48y/iW7xSrPo/KTN0I5RIh9kMblMjIgdelrCdsz8XsRPk8wOQ6gn4iPwx4vevMRfAOnhREQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '6024b4944041d7b1e2d5671caba23025';
+$idPush = 'U169659caa82c674a47fb9d6fbe2cabfc'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
